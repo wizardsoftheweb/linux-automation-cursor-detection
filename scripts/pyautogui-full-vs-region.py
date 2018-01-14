b8bcea6026001eaeda5087fee94f6288862b51b0
CJ Harries <cj@wizardsoftheweb.pro>
2018-01-14 02:47:46 -0600

    Create screenshot comparison example

---
+++ scripts/pyautogui-full-vs-region.py
@@ -0,0 +1,41 @@
+"""This file illustrates using pyautogui to check a pixel's color"""
+
+from __future__ import print_function
+import time
+
+import pyautogui
+
+print('Using a region')
+START = time.time()
+SCREEN_WIDTH, SCREEN_HEIGHT = pyautogui.size()
+MOUSE_X, MOUSE_Y = pyautogui.position()
+PIXEL = pyautogui.screenshot(
+    region=(
+        MOUSE_X, MOUSE_Y, 1, 1
+    )
+)
+COLOR = PIXEL.getcolors()
+END = time.time()
+
+print("Screen: %dx%d" % (SCREEN_WIDTH, SCREEN_HEIGHT))
+print("Mouse: (%d,%d)" % (MOUSE_X, MOUSE_Y))
+print("RGB: %s" % (COLOR[0][1].__str__()))
+print("Start: %s" % (START))
+print("End: %s" % (END))
+print("Difference: %s" % (END - START))
+
+print("=" * 10)
+
+print('Full screen')
+START = time.time()
+SCREEN_WIDTH, SCREEN_HEIGHT = pyautogui.size()
+MOUSE_X, MOUSE_Y = pyautogui.position()
+PIXEL = pyautogui.screenshot()
+COLOR = PIXEL.getcolors()
+END = time.time()
+
+print("Screen: %dx%d" % (SCREEN_WIDTH, SCREEN_HEIGHT))
+print("Mouse: (%d,%d)" % (MOUSE_X, MOUSE_Y))
+print("Start: %s" % (START))
+print("End: %s" % (END))
+print("Difference: %s" % (END - START))
