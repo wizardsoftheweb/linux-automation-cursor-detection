996bef5ebaee81b6f234388226bf380fde080ef8
CJ Harries <cj@wizardsoftheweb.pro>
2018-01-14 05:46:10 -0600

    Create xwd dangerous example

---
+++ scripts/xmd-convert-dangerous.py
@@ -0,0 +1,17 @@
+"""This file contains code to monitor the desktop environment"""
+
+import subprocess
+import time
+
+START = time.time()
+OUTPUT = subprocess.check_output(
+    'eval $(xdotool getmouselocation --shell); '
+    'xwd -root -screen -silent '
+    '| convert xwd:- -crop "1x1+$X+$Y" text:-',
+    shell=True
+)
+END = time.time()
+print OUTPUT
+print "START: %s" % (START)
+print "End: %s" % (END)
+print "Difference: %s" % (END - START)
